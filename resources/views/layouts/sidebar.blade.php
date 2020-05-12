1d5ddddb3a480eff0d27a567112bc3a46ea3b2c9
Cesar Alejandro Mora Rodriguez <cesarmora014@gmail.com>
2020-05-12 17:24:25 -0500

    layout admi panel

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,273 @@
+<nav id="sidebar" data-simplebar="init"><div class="simplebar-wrapper" style="margin: 0px;"><div class="simplebar-height-auto-observer-wrapper"><div class="simplebar-height-auto-observer"></div></div><div class="simplebar-mask"><div class="simplebar-offset" style="right: -17px; bottom: 0px;"><div class="simplebar-content-wrapper" style="height: 100%; overflow: hidden scroll;"><div class="simplebar-content" style="padding: 0px;">
+                <!-- Sidebar Content -->
+                <div class="sidebar-content">
+                    <!-- Side Header -->
+                    <div class="content-header content-header-fullrow px-15">
+                        <!-- Mini Mode -->
+                        <div class="content-header-section sidebar-mini-visible-b">
+                            <!-- Logo -->
+                            <span class="content-header-item font-w700 font-size-xl float-left animated fadeIn">
+                                <span class="text-dual-primary-dark">c</span><span class="text-primary">b</span>
+                            </span>
+                            <!-- END Logo -->
+                        </div>
+                        <!-- END Mini Mode -->
+
+                        <!-- Normal Mode -->
+                        <div class="content-header-section text-center align-parent sidebar-mini-hidden">
+                            <!-- Close Sidebar, Visible only on mobile screens -->
+                            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                            <button type="button" class="btn btn-circle btn-dual-secondary d-lg-none align-v-r" data-toggle="layout" data-action="sidebar_close">
+                                <i class="fa fa-times text-danger"></i>
+                            </button>
+                            <!-- END Close Sidebar -->
+
+                            <!-- Logo -->
+                            <div class="content-header-item">
+                                <a class="link-effect font-w700" href="index.html">
+                                    <i class="si si-fire text-primary"></i>
+                                    <span class="font-size-xl text-dual-primary-dark">code</span><span class="font-size-xl text-primary">base</span>
+                                </a>
+                            </div>
+                            <!-- END Logo -->
+                        </div>
+                        <!-- END Normal Mode -->
+                    </div>
+                    <!-- END Side Header -->
+
+                    <!-- Side User -->
+                    <div class="content-side content-side-full content-side-user px-10 align-parent">
+                        <!-- Visible only in mini mode -->
+                        <div class="sidebar-mini-visible-b align-v animated fadeIn">
+                            <img class="img-avatar img-avatar32" src="assets/media/avatars/avatar15.jpg" alt="">
+                        </div>
+                        <!-- END Visible only in mini mode -->
+
+                        <!-- Visible only in normal mode -->
+                        <div class="sidebar-mini-hidden-b text-center">
+                            <a class="img-link" href="be_pages_generic_profile.html">
+                                <img class="img-avatar" src="assets/media/avatars/avatar15.jpg" alt="">
+                            </a>
+                            <ul class="list-inline mt-10">
+                                <li class="list-inline-item">
+                                    <a class="link-effect text-dual-primary-dark font-size-xs font-w600 text-uppercase" href="be_pages_generic_profile.html">J. Smith</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                                    <a class="link-effect text-dual-primary-dark" data-toggle="layout" data-action="sidebar_style_inverse_toggle" href="javascript:void(0)">
+                                        <i class="si si-drop"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="link-effect text-dual-primary-dark" href="op_auth_signin.html">
+                                        <i class="si si-logout"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- END Visible only in normal mode -->
+                    </div>
+                    <!-- END Side User -->
+
+                    <!-- Side Navigation -->
+                    <div class="content-side content-side-full">
+                        <ul class="nav-main">
+                            <li>
+                                <a href="be_pages_dashboard.html"><i class="si si-cup"></i><span class="sidebar-mini-hide">Dashboard</span></a>
+                            </li>
+                            <li>
+                                <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-badge"></i><span class="sidebar-mini-hide">Page Kits</span></a>
+                                <ul>
+                                    <li>
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Dashboards</span></a>
+                                        <ul>
+                                            <li>
+                                                <a href="be_pages_dashboard2.html"><span class="sidebar-mini-hide">Dashboard 2</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_dashboard3.html"><span class="sidebar-mini-hide">Dashboard 3</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_dashboard4.html"><span class="sidebar-mini-hide">Dashboard 4</span></a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Hosting</span></a>
+                                        <ul>
+                                            <li>
+                                                <a href="be_pages_hosting_dashboard.html">Dashboard</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_hosting_emails.html">Emails</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_hosting_account.html">Account</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_hosting_support.html">Support</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Real Estate</span></a>
+                                        <ul>
+                                            <li>
+                                                <a href="be_pages_real_estate_dashboard.html">Dashboard</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_real_estate_listing.html">Listing</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_real_estate_listing_new.html">Add New Listing</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Crypto</span></a>
+                                        <ul>
+                                            <li>
+                                                <a href="be_pages_crypto_dashboard.html">Dashboard</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_crypto_buy_sell.html">Buy/Sell</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_crypto_wallets.html">Wallets</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_crypto_settings.html">Settings</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">e-Commerce</span></a>
+                                        <ul>
+                                            <li>
+                                                <a href="be_pages_ecom_dashboard.html">Dashboard</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_ecom_orders.html">Orders</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_ecom_order.html">Order</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_ecom_products.html">Products</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_ecom_product_edit.html">Product Edit</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_ecom_customer.html">Customer</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">e-Learning</span></a>
+                                        <ul>
+                                            <li>
+                                                <a href="be_pages_elearning_courses.html">Courses</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_elearning_course.html">Course</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_elearning_lesson.html">Lesson</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Forum</span></a>
+                                        <ul>
+                                            <li>
+                                                <a href="be_pages_forum_categories.html">Categories</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_forum_topics.html">Topics</a>
+                                            </li>
+                                            <li>
+                                                <a href="be_pages_forum_discussion.html">Discussion</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Alternative Dashboards</span></a>
+                                        <ul>
+                                            <li>
+                                                <a href="db_classic.html"><span class="sidebar-mini-hide">Classic</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="db_clean.html"><span class="sidebar-mini-hide">Clean</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="db_social.html"><span class="sidebar-mini-hide">Social</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="db_corporate.html"><span class="sidebar-mini-hide">Corporate</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="db_minimal.html"><span class="sidebar-mini-hide">Minimal</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="db_pop.html"><span class="sidebar-mini-hide">Pop</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="db_dark.html"><span class="sidebar-mini-hide">Dark</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="db_medical.html"><span class="sidebar-mini-hide">Medical</span></a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Boxed Backend</span></a>
+                                        <ul>
+                                            <li>
+                                                <a href="bd_dashboard.html">Dashboard</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_search.html">Search</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_variations_hero_simple_1.html">Hero Simple 1</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_variations_hero_simple_2.html">Hero Simple 2</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_variations_hero_simple_3.html">Hero Simple 3</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_variations_hero_simple_4.html">Hero Simple 4</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_variations_hero_image_1.html">Hero Image 1</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_variations_hero_image_2.html">Hero Image 2</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_variations_hero_image_3.html">Hero Image 3</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_variations_hero_image_4.html">Hero Image 4</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_variations_hero_video_1.html">Hero Video 1</a>
+                                            </li>
+                                            <li>
+                                                <a href="bd_variations_hero_video_2.html">Hero Video 2</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-main-heading"><span class="sidebar-mini-visible">UI</span><span class="sidebar-mini-hidden">User Interface</span></li>
+                            
+                        </ul>
+                    </div>
+                    <!-- END Side Navigation -->
+                </div>
+                <!-- Sidebar Content -->
+            </div></div></div></div><div class="simplebar-placeholder" style="width: auto; height: 1362px;"></div></div><div class="simplebar-track simplebar-horizontal" style="visibility: hidden;"><div class="simplebar-scrollbar" style="transform: translate3d(0px, 0px, 0px); display: none;"></div></div><div class="simplebar-track simplebar-vertical" style="visibility: visible;"><div class="simplebar-scrollbar" style="height: 316px; transform: translate3d(0px, 0px, 0px); display: block;"></div></div></nav>
