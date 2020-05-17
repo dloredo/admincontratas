7bf06017878f39d0b34e11be78d9a5c69248f3fc
Cesar Alejandro Mora Rodriguez <cesarmora014@gmail.com>
2020-05-17 01:38:02 -0500

    menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,110 +1,121 @@
-<nav id="sidebar" data-simplebar="init"><div class="simplebar-wrapper" style="margin: 0px;"><div class="simplebar-height-auto-observer-wrapper"><div class="simplebar-height-auto-observer"></div></div><div class="simplebar-mask"><div class="simplebar-offset" style="right: -17px; bottom: 0px;"><div class="simplebar-content-wrapper" style="height: 100%; overflow: hidden scroll;"><div class="simplebar-content" style="padding: 0px;">
-                <!-- Sidebar Content -->
-                <div class="sidebar-content">
-                    <!-- Side Header -->
-                    <div class="content-header content-header-fullrow px-15">
-                        <!-- Mini Mode -->
-                        <div class="content-header-section sidebar-mini-visible-b">
-                            <!-- Logo -->
-                            <span class="content-header-item font-w700 font-size-xl float-left animated fadeIn">
-                                <span class="text-dual-primary-dark">c</span><span class="text-primary">b</span>
-                            </span>
-                            <!-- END Logo -->
-                        </div>
-                        <!-- END Mini Mode -->
+<nav id="sidebar" data-simplebar="init">
+    <div class="simplebar-wrapper" style="margin: 0px;">
+        <div class="simplebar-height-auto-observer-wrapper">
+            <div class="simplebar-height-auto-observer"></div>
+        </div>
+        <div class="simplebar-mask">
+            <div class="simplebar-offset" style="right: -17px; bottom: 0px;">
+                <div class="simplebar-content-wrapper" style="height: 100%; overflow: hidden scroll;">
+                    <div class="simplebar-content" style="padding: 0px;">
+                        <!-- Sidebar Content -->
+                        <div class="sidebar-content">
+                            <!-- Side Header -->
+                            <div class="content-header content-header-fullrow px-15">
+                                <!-- Mini Mode -->
+                                <div class="content-header-section sidebar-mini-visible-b">
+                                    <!-- Logo -->
+                                    <span class="content-header-item font-w700 font-size-xl float-left animated fadeIn">
+                                        <span class="text-dual-primary-dark">c</span><span class="text-primary">b</span>
+                                    </span>
+                                    <!-- END Logo -->
+                                </div>
+                                <!-- END Mini Mode -->
 
-                        <!-- Normal Mode -->
-                        <div class="content-header-section text-center align-parent sidebar-mini-hidden">
-                            <!-- Close Sidebar, Visible only on mobile screens -->
-                            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-                            <button type="button" class="btn btn-circle btn-dual-secondary d-lg-none align-v-r" data-toggle="layout" data-action="sidebar_close">
-                                <i class="fa fa-times text-danger"></i>
-                            </button>
-                            <!-- END Close Sidebar -->
+                                <!-- Normal Mode -->
+                                <div class="content-header-section text-center align-parent sidebar-mini-hidden">
+                                    <!-- Close Sidebar, Visible only on mobile screens -->
+                                    <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                                    <button type="button" class="btn btn-circle btn-dual-secondary d-lg-none align-v-r" data-toggle="layout" data-action="sidebar_close">
+                                        <i class="fa fa-times text-danger"></i>
+                                    </button>
+                                    <!-- END Close Sidebar -->
 
-                            <!-- Logo -->
-                            <div class="content-header-item">
-                                <a class="link-effect font-w700" href="index.html">
-                                    <i class="si si-fire text-primary"></i>
-                                    <span class="font-size-xl text-dual-primary-dark">code</span><span class="font-size-xl text-primary">base</span>
-                                </a>
+                                    <!-- Logo -->
+                                    <div class="content-header-item">
+                                        <a class="link-effect font-w700" href="index.html">
+                                            <i class="si si-fire text-primary"></i>
+                                            <span class="font-size-xl text-dual-primary-dark">code</span><span class="font-size-xl text-primary">base</span>
+                                        </a>
+                                    </div>
+                                    <!-- END Logo -->
+                                </div>
+                                <!-- END Normal Mode -->
                             </div>
-                            <!-- END Logo -->
-                        </div>
-                        <!-- END Normal Mode -->
-                    </div>
-                    <!-- END Side Header -->
+                            <!-- END Side Header -->
 
-                    <!-- Side User -->
-                    <div class="content-side content-side-full content-side-user px-10 align-parent">
-                        <!-- Visible only in mini mode -->
-                        <div class="sidebar-mini-visible-b align-v animated fadeIn">
-                            <img class="img-avatar img-avatar32" src="assets/media/avatars/avatar15.jpg" alt="">
-                        </div>
-                        <!-- END Visible only in mini mode -->
+                            <!-- Side User -->
+                            <div class="content-side content-side-full content-side-user px-10 align-parent" style="height: inherit;">
+                                <!-- ········ -->
+                                <div class="sidebar-mini-visible-b align-v animated fadeIn">
+                                    <img class="img-avatar img-avatar32" src="assets/media/avatars/avatar15.jpg" alt="">
+                                </div>
+                                <!-- END Visible only in mini mode -->
 
-                        <!-- Visible only in normal mode -->
-                        <div class="sidebar-mini-hidden-b text-center">
-                            <a class="img-link" href="be_pages_generic_profile.html">
+                                <!-- Visible only in normal mode -->
+                                <div class="sidebar-mini-hidden-b text-center">
+                                    <!--##################DESCOMENTAR ESTO PARA LA IMAGEN DEL PERFIL SI SE OCUPA#########################################
+                                <a class="img-link" href="be_pages_generic_profile.html">
                                 <img class="img-avatar" src="{{ asset('assets/media/avatars/avatar15.jpg') }}" alt="">
-                            </a>
-                            <ul class="list-inline mt-10">
-                                <li class="list-inline-item">
-                                    <a class="link-effect text-dual-primary-dark font-size-xs font-w600 text-uppercase" href="be_pages_generic_profile.html">{{Auth::user()->nombres}}</a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a class="link-effect text-dual-primary-dark" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            </a>-->
+                                    <ul class="list-inline mt-10">
+                                        <li class="list-inline-item">
+                                            <a class="link-effect text-dual-primary-dark font-size-xs font-w600 text-uppercase" href="be_pages_generic_profile.html">{{Auth::user()->nombres}}</a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="link-effect text-dual-primary-dark" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        <i class="si si-logout"></i>
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- END Visible only in normal mode -->
-                    </div>
-                    <!-- END Side User -->
+                                                <i class="si si-logout"></i>
+                                            </a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <!-- END Visible only in normal mode -->
+                            </div>
+                            <!-- END Side User -->
 
-                    <!-- Side Navigation -->
-                    <div class="content-side content-side-full">
-                        <ul class="nav-main">
-                            <li>
-                                <a href="be_pages_dashboard.html"><i class="si si-cup"></i><span class="sidebar-mini-hide">Dashboard</span></a>
-                            </li>
-                            <li>
-                                <a href="{{ route('vista.clientes') }}"><i class="fa fa-address-book-o"></i><span class="sidebar-mini-hide">Clientes</span></a>
-                            </li>
+                            <!-- Side Navigation -->
+                            <div class="content-side content-side-full">
+                                <ul class="nav-main">
+                                    <li class="nav-main-heading"><span class="sidebar-mini-visible">UI</span><span class="sidebar-mini-hidden">MENU</span></li>
 
-                            <li>
-                                <a href="{{ route('vista.usuarios') }}"><i class="fa fa-users"></i><span class="sidebar-mini-hide">Usuarios</span></a>
-                            </li>
-                            <li>
-                                <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-badge"></i><span class="sidebar-mini-hide">Control</span></a>
-                                <ul>
                                     <li>
-                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#"><span class="sidebar-mini-hide">Dashboards</span></a>
-                                        <ul>
-                                            <li>
-                                                <a href="be_pages_dashboard2.html"><span class="sidebar-mini-hide">Dashboard 2</span></a>
-                                            </li>
-                                            <li>
-                                                <a href="be_pages_dashboard3.html"><span class="sidebar-mini-hide">Dashboard 3</span></a>
-                                            </li>
-                                            <li>
-                                                <a href="be_pages_dashboard4.html"><span class="sidebar-mini-hide">Dashboard 4</span></a>
-                                            </li>
-                                        </ul>
+                                        <a href="be_pages_dashboard.html"><i class="si si-home"></i><span class="sidebar-mini-hide">Principal</span></a>
                                     </li>
-                                    
+                                    <li>
+                                        <a href="be_pages_dashboard.html"><i class="fa fa-money"></i><span class="sidebar-mini-hide">Capital</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ route('vista.clientes') }}"><i class="fa fa-address-book-o"></i><span class="sidebar-mini-hide">Clientes</span></a>
+                                    </li>
+
+
+                                    <li class="nav-main-heading"><span class="sidebar-mini-visible">UI</span><span class="sidebar-mini-hidden">Sistema</span></li>
+                                    <li>
+                                        <a href="{{ route('vista.usuarios') }}"><i class="fa fa-users"></i><span class="sidebar-mini-hide">Usuarios</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ route('vista.usuarios') }}"><i class="si si-info"></i><span class="sidebar-mini-hide">Información</span></a>
+                                    </li>
+
+
                                 </ul>
-                            </li>
-                            <li class="nav-main-heading"><span class="sidebar-mini-visible">UI</span><span class="sidebar-mini-hidden">User Interface</span></li>
-                            
-                        </ul>
+                            </div>
+                            <!-- END Side Navigation -->
+                        </div>
+                        <!-- Sidebar Content -->
                     </div>
-                    <!-- END Side Navigation -->
                 </div>
-                <!-- Sidebar Content -->
-            </div></div></div></div><div class="simplebar-placeholder" style="width: auto; height: 1362px;"></div></div><div class="simplebar-track simplebar-horizontal" style="visibility: hidden;"><div class="simplebar-scrollbar" style="transform: translate3d(0px, 0px, 0px); display: none;"></div></div><div class="simplebar-track simplebar-vertical" style="visibility: visible;"><div class="simplebar-scrollbar" style="height: 316px; transform: translate3d(0px, 0px, 0px); display: block;"></div></div></nav>
+            </div>
+        </div>
+        <div class="simplebar-placeholder" style="width: auto; height: 1362px;"></div>
+    </div>
+    <div class="simplebar-track simplebar-horizontal" style="visibility: hidden;">
+        <div class="simplebar-scrollbar" style="transform: translate3d(0px, 0px, 0px); display: none;"></div>
+    </div>
+    <div class="simplebar-track simplebar-vertical" style="visibility: visible;">
+        <div class="simplebar-scrollbar" style="height: 316px; transform: translate3d(0px, 0px, 0px); display: block;"></div>
+    </div>
+</nav>
---
+++ resources/views/principal/principal.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.layout')
+
+@section('main')
+
+@if(Session::get('saved'))
+<div class="alert alert-success alert-dismissable" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">×</span>
+    </button>
+    <h3 class="alert-heading font-size-h4 font-w400">Correcto</h3>
+    <p class="mb-0">{{Session::get('message')}}</p>
+</div>
+@endif
+
+<h2 class="content-heading">Control de usuarios</h2>
+<div class="block">
+    <div class="block-header block-header-default">
+        <h3 class="block-title">Clientes</h3>
+        <div style="float: right">
+            <a href="{{ route('vista.agregarUsuario') }}"><button type="button" class="btn btn-primary">Añadir nuevo usuario</button></a>
+        </div>
+    </div>
+    <div class="block-content block-content-full">
+        <!-- DataTables functionality is initialized with .js-dataTable-full class in js/pages/be_tables_datatables.min.js which was auto compiled from _es6/pages/be_tables_datatables.js -->
+        <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th class="d-none d-sm-table-cell">Direccion</th>
+                    <th class="d-none d-sm-table-cell" style="width: 15%;">Telefono</th>
+                    <th class="d-none d-sm-table-cell" style="width: 15%;">Rol</th>
+                    <th class="d-none d-sm-table-cell" style="width: 15%;">Estatus</th>
+                    <th class="text-center" style="width: 15%;">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($usuarios as $usuario)
+                    <tr>
+                        <td class="font-w600">{{$usuario->nombres}} {{$usuario->apellidos}}</td>
+                        <td class="d-none d-sm-table-cell">{{$usuario->direccion}}</td>
+                        <td class="d-none d-sm-table-cell">{{$usuario->telefono}}</td>
+                        <td class="d-none d-sm-table-cell">{{$usuario->rol->rol}}</td>
+                        <td class="d-none d-sm-table-cell">{{$usuario->activo}}</td>
+                        <td class="text-center">
+                            <a href="{{ route('edit.cambiarEstatus' , [ 'id' => $usuario->id, 'estatus' => $usuario->activo]) }}" type="button" class="btn btn-sm {{($usuario->activo == 'Activo')? 'btn-warning' : 'btn-success' }}">{{($usuario->activo == 'Activo')? 'Inactivar' : 'Activar' }}</a>
+                            <button style="margin-top:10px;" type="button" class="btn btn-sm btn-danger"  onclick="showModal({{$usuario->id}}, '{{$usuario->nombres}}')">Eliminar</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+<div class="modal fade" id="modal-popin" tabindex="-1" role="dialog" aria-labelledby="modal-popin" aria-modal="true">
+            <div class="modal-dialog modal-dialog-popin" role="document">
+                <div class="modal-content">
+                    <div class="block block-themed block-transparent mb-0">
+                        <div class="block-header bg-primary-dark">
+                            <h3 class="block-title">Eliminar este usuario</h3>
+                            <div class="block-options">
+                                <button type="button" class="btn-block-option" data-dismiss="modal" aria-label="Close">
+                                    <i class="si si-close"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="block-content">
+                            <p id="modalTextContent">¿Esta seguro de eliminar a este usuario?</p>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-alt-secondary" data-dismiss="modal">Cancelar</button>
+                        <a id="linkEliminar" type="button" class="btn btn-alt-danger">
+                            <i class="fa fa-check"></i> Eliminar
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
+
+@section('styles')
+
+@endsection
+
+
+@section('scripts')
+
+<script>
+
+
+</script>
+
+@endsection
