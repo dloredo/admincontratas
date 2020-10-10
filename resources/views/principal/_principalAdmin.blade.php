40bb7b8f758730718867f2ba9794dccdcca370c3
CesarMora <cesarmora014@gmail.com>
2020-10-10 00:18:45 -0500

    iconos

--- resources/views/principal/_principalAdmin.blade.php
+++ resources/views/principal/_principalAdmin.blade.php
@@ -4,7 +4,7 @@
         <a class="block block-link-shadow text-right" href="javascript:void(0)">
             <div class="block-content block-content-full clearfix">
                 <div class="float-left mt-10 d-none d-sm-block">
-                    <i class="si si-bag fa-3x text-body-bg-dark"></i>
+                    <i class="si si-wallet fa-3x text-body-bg-dark"></i>
                 </div>
                 <div class="font-size-h3 font-w600"><span data-toggle="countTo" data-speed="1000" data-to="780" class="js-count-to-enabled"><?php echo "$" . number_format(round(((float)$saldo_esperado)),2,'.',',');?></span></div>
                 <div class="font-size-sm font-w600 text-uppercase text-muted">SALDO ESPERADO</div>
@@ -15,7 +15,7 @@
         <a class="block block-link-shadow text-right" href="javascript:void(0)">
             <div class="block-content block-content-full clearfix">
                 <div class="float-left mt-10 d-none d-sm-block">
-                    <i class="si si-wallet fa-3x text-body-bg-dark"></i>
+                    <i class="si si-users fa-3x text-body-bg-dark"></i>
                 </div>
                 <div class="font-size-h3 font-w600"><span data-toggle="countTo" data-speed="1000" data-to="780" class="js-count-to-enabled">{{ $total_clientes }}</span></div>
                 <div class="font-size-sm font-w600 text-uppercase text-muted">Clientes</div>
@@ -26,7 +26,7 @@
         <a class="block block-link-shadow text-right" href="javascript:void(0)">
             <div class="block-content block-content-full clearfix">
                 <div class="float-left mt-10 d-none d-sm-block">
-                    <i class="si si-envelope-open fa-3x text-body-bg-dark"></i>
+                    <i class="fa fa-usd fa-3x text-body-bg-dark"></i>
                 </div>
                 <div class="font-size-h3 font-w600 js-count-to-enabled" data-toggle="countTo" data-speed="1000" data-to="15">{{ $total_contratas }}</div>
                 <div class="font-size-sm font-w600 text-uppercase text-muted">Contratas</div>
@@ -37,7 +37,7 @@
         <a class="block block-link-shadow text-right" href="javascript:void(0)">
             <div class="block-content block-content-full clearfix">
                 <div class="float-left mt-10 d-none d-sm-block">
-                    <i class="si si-users fa-3x text-body-bg-dark"></i>
+                    <i class="fa fa-user fa-3x text-body-bg-dark"></i>
                 </div>
                 <div class="font-size-h3 font-w600 js-count-to-enabled" data-toggle="countTo" data-speed="1000" data-to="4252">{{ $total_cobradores }}</div>
                 <div class="font-size-sm font-w600 text-uppercase text-muted">Cobradores</div>
