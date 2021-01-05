65be9b2d463fc4061eb1ebe0c4693c95cb90bf76
dloredo <dloredo@ucol.mx>
2021-01-05 14:25:59 -0600

    tabla cortes fix

--- resources/views/capital/_tablaCortes.blade.php
+++ resources/views/capital/_tablaCortes.blade.php
@@ -1,22 +1,21 @@
 <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
     <thead>
-        <tr>
-            <th>capital acumulado</th>
-            <th class="d-none d-sm-table-cell">saldo efectivo</th>
-            <th class="d-none d-sm-table-cell">capital parcial</th>
+        <tr style="text-align: center;">
+            <th>capital acum. anterior</th>
             <th class="d-none d-sm-table-cell">Comisiones</th>
+            <th class="d-none d-sm-table-cell">Gastos</th>
+            <th class="d-none d-sm-table-cell">Capital acum. nuevo</th>
             <th class="d-none d-sm-table-cell">Fecha de corte</th>
         </tr>
     </thead>
     <tbody>
 
     @foreach($cortes as $corte)
-
-        <tr>
-            <td>{{$corte->capital_acumulado}}</td>
-            <td>{{$corte->saldo_efectivo}}</td>
-            <td>{{$corte->capital_parcial}}</td>
-            <td>{{$corte->comisiones}}</td>
+        <tr style="text-align: center;">
+            <td><?php echo "$" . number_format(round(((float)$corte->capital_acumulado)),2,'.',',');?></td>
+            <td><?php echo "$" . number_format(round(((float)$corte->comisiones)),2,'.',',');?></td>
+            <td><?php echo "$" . number_format(round(((float)$corte->gastos)),2,'.',',');?></td>
+            <td><?php echo "$" . number_format(round(((float)$corte->capital_acumulado + $corte->comisiones - $corte->gastos)),2,'.',',');?></td>
             <td>{{$corte->created_at}}</td>
         </tr>
     @endforeach
